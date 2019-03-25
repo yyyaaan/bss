4e135183e54926642b9af5dda0bf1d830ecf00dd
Yan Pan <yanpan@yan-fi-server.europe-north1-c.c.yyyaaannn.internal>
2019-03-25 06:56:01 +0000

    latest batch mode with local

--- aRunBatch.R
+++ aRunBatch.R
@@ -3,8 +3,9 @@ library(parallel)
 library(JADE)
 remove(list = ls()) # clear environment
 source("rfun.R"); source("rlab_x.R")
+source("rsim.R")
 options(stringsAsFactors = FALSE)
-#N <- 1000; p <- 4; id = "TEST"; append = TRUE
+#N <- 10000; p <- 4; id = "TEST"; append = TRUE
 
 
 # single sim with FIXED source --------------------------------------------
@@ -13,17 +14,18 @@ do_one_sim <- function(p = 9, N = 1e3, id = "TEST"){
   
   # get source and mix
   Omega   <- matrix(runif(p^2, 1, 10), ncol = p)
-  Epsilon <- 1e-5 * matrix(runif(p^2, 1, 10), ncol = p)
-  z <- NULL
-  for (i in 1:p){
-    rand <- sample(1:3, 1)
-    if (rand == 1) var <- arima.sim(list(ar=runif(1,-1,1)),N)
-    if (rand == 2) var <- arima.sim(list(ma=runif(1,-1,1)),N)
-    if (rand == 3) var <- sin((1:N)/(N/100))
-    z   <- cbind(z, var)
-  }
+  Epsilon <- 1e-4 * matrix(runif(p^2, 1, 10), ncol = p)
+  # z <- NULL
+  # for (i in 1:p){
+  #   rand <- sample(1:3, 1)
+  #   if (rand == 1) var <- arima.sim(list(ar=runif(1,-1,1)),N)
+  #   if (rand == 2) var <- arima.sim(list(ma=runif(1,-1,1)),N)
+  #   if (rand == 3) var <- sin((1:N)/(N/100))
+  #   z   <- cbind(z, var)
+  # }
+  z <- sim_good_sources(N, p)
   x <- tvmix(z, Omega, Epsilon)
-  
+  x <- scale(x, scale = FALSE)
   #save source
   #save_signal(z, id, "true_signal")
   #save_signal(z, id, "mixture")
@@ -31,20 +33,28 @@ do_one_sim <- function(p = 9, N = 1e3, id = "TEST"){
   
   # call all methods
   for (i in 1:8) {
-    if(i == 1) bss_res <- JADE::SOBI(x, k = 6)
-    if(i == 2) bss_res <- tvsobi  (x, lag.max = 6, TRUE)
-    if(i == 3) bss_res <- tvsobi  (x, lag.max = 6, FALSE)
-    if(i == 4) bss_res <- ltvsobi (x, lags = 6, TRUE, TRUE)
-    if(i == 5) bss_res <- ltvsobi (x, lags = 6, TRUE, FALSE)
-    if(i == 6) bss_res <- ltvsobi (x, lags = 6, FALSE, TRUE)
-    if(i == 7) bss_res <- ltvsobi (x, lags = 6, FALSE, FALSE)
-    if(i == 8) bss_res <- ltvsobi2(x, lags = 6)
+    flag <- TRUE
+    tryCatch({
+      if(i == 1) bss_res <- JADE::SOBI(x, k = 6)
+      if(i == 2) bss_res <- tvsobi  (x, lag.max = 6, TRUE)
+      if(i == 3) bss_res <- tvsobi  (x, lag.max = 6, FALSE)
+      if(i == 4) bss_res <- ltvsobi (x, lags = 6, quadratic = TRUE,  fix_symmetry = TRUE,  use_vec = FALSE)
+      if(i == 5) bss_res <- ltvsobi (x, lags = 6, quadratic = TRUE,  fix_symmetry = FALSE, use_vec = FALSE)
+      if(i == 6) bss_res <- ltvsobi (x, lags = 6, quadratic = FALSE, fix_symmetry = TRUE,  use_vec = FALSE)
+      if(i == 7) bss_res <- ltvsobi (x, lags = 6, quadratic = FALSE, fix_symmetry = FALSE, use_vec = FALSE)
+      if(i == 8) bss_res <- ltvsobi2(x, lags = 6, use_vec = FALSE)
+    }, error = function(e) {
+      print(paste("skip to next due to", e))
+      flag <<- FALSE
+    })
     
-    save_estimator(bss_res, id)
-    #save_restored(bss_res, id)
-    benchmarks <- SIR_all(bss_res, Omega, Epsilon, z)
-    remove(bss_res)
-    save_eval(benchmarks, id)
+    if(flag) {
+      #save_estimator(bss_res, id)
+      #save_restored(bss_res, id)
+      benchmarks <- SIR_all(bss_res, Omega, Epsilon, z)
+      remove(bss_res)
+      save_eval(benchmarks, id)
+    }
   }  
 }
 
@@ -116,22 +126,18 @@ save_restored <- function(bss_res, id){
 # function for Parallel Computing -----------------------------------------
 
 do_full_set <- function(rid = "XXXX"){
-  Ns <- 50 * 2 ^ {1:11}
-  ps  <- 3:9
-  for(N in Ns) for(p in ps) {
+  Ns <- 50 * 2 ^ {11:1}
+  ps  <- 9:3
+  for(p in ps) for(N in Ns) {
     tryCatch(do_one_sim(p, N, paste0( as.numeric(Sys.Date()), "ID", rid, "D", p, "N", N)),
              error = function(e) print(paste("error occured and ignored:", e)))
   }
   print(paste("BATCH", rid, "Completed"))
 }
 
-for(i in 1:30){
-  
-}
 
-for (i in 1:100) do_full_set(9000 + i)
+for (i in 1:99) do_full_set(paste0("NEW", 6000 + i))
 # mclapply(9001:9399, function(rid) do_full_set(rid), mc.cores = detectCores())
-
 
 # file processing ---------------------------------------------------------
 
@@ -146,3 +152,26 @@ combined_files <- function(indicator = "benchmarks"){
   }
   saveRDS(df, file = paste0(indicator, ".rds"))
 }
+
+# benchmarking plotting ---------------------------------------------------
+plotting <- function() {
+  library(tidyverse)
+  combined_files()
+  benchmarks <- readRDS("benchmarks.rds")
+  benchmarks$m = str_remove(benchmarks$method, " NearestSPD")
+  benchmarks$simple_method = word(benchmarks$method)
+  benchmarks$N = str_extract(benchmarks$id, "N[:digit:]{3,8}") %>% str_remove("N") %>% parse_integer()
+  benchmarks$p = str_extract(benchmarks$id, "D[:digit:]N") %>% str_remove("D") %>% str_remove("N") %>% parse_integer()
+  benchmarks$logN = log(benchmarks$N)
+  
+  benchmarks %>%
+    filter(!is.na(value)) %>%
+    filter(p <= 6) %>%
+    group_by(criteria, simple_method, N, p, logN) %>%
+    summarise_at("value", mean) %>%
+    ggplot(aes(x = logN, y = value, color = simple_method)) +
+    geom_line() +
+    facet_grid(criteria ~ p, scales = "free_y")
+  
+}
+print("Compeleted!")
